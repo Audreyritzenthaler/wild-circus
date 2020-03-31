dc2c7ce0212367b2550c3e3fc46e08203e9e64fe
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-03-31 13:18:46 +0200

    add composant navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="wrapper">
+            <ul>
+                <li>
+                    <a href="https://github.com/Audreyritzenthaler">Performances</a> 
+                </li>
+                <li>
+                    <a href="https://github.com/Audreyritzenthaler">About Us</a> 
+                </li>
+                <li>
+                    <a href="https://github.com/Audreyritzenthaler">Prices</a> 
+                </li>
+                <li>
+                    <a href="https://github.com/Audreyritzenthaler">Contact</a>
+                </li>
+        </ul>
+        </div>
+    )
+}
+
+
+export default Navbar;
