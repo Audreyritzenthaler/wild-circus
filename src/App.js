8b2a5861a93cf1c267829e7450bf292b88ab6cee
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-01 18:31:24 +0200

    lolilol

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import Header from './components/Header';
 import AboutUs from './components/AboutUs';
+import Prices from './components/Prices';
 import './App.css';
 import SectionPerformances from './components/SectionPerformances'
 
@@ -21,6 +22,7 @@ function App() {
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
                 dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                 onsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu."/>
+      <Prices />
     </div>
   );
 }
--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,31 +1,11 @@
 import React from 'react';
+import './Prices.css';
 
 
 const Prices = () => {
     return (
         <div className="mytab">
-            <table>
-                <thead>
-                    <tr>
-                        <th colSpan="">Prices</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td>Adult</td>
-                        <td>Children -12 years old</td>
-                        <td>group (+10 person)</td>
-                        <td>School</td>
-                    </tr>
-                    <tr>
-
-                    </tr>
-                    <tr>
-                        
-                    </tr>
-                </tbody>
-            </table>
+            
         </div>
     )
 }
