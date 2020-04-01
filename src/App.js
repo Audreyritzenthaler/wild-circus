5eda20ecaca451fddc321fabe46f547c21afe62e
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-01 15:00:05 +0200

    agencement components in the final site

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Header from './components/Header';
-import SectionPerformances from './components/SectionPerformances'
 import './App.css';
+import SectionPerformances from './components/SectionPerformances'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
-      <SectionPerformances />
+      <div className="card">
+        <SectionPerformances />
+      </div>
     </div>
   );
 }
--- src/components/SectionTypePerf.js
+++ src/components/SectionTypePerf.js
@@ -3,13 +3,13 @@ import './SectionPerformances.css';
 
 const SectionType = (props) => {
     return (
-        <div className="cards">
-            <img src={props.image}/>
-            <div>
-                <h4>{props.title}</h4>
-                <p className="textuel" >{props.text}</p>
+            <div className="cards">
+                    <img src={props.image}/>
+                    <div className="divPerf">
+                        <h4>{props.title}</h4>
+                        <p className="textuel" >{props.text}</p>
+                    </div>
             </div>
-        </div>
     )
 }
 
