594caf5ecf1c411b2dabc8fc9955aa5626a32b43
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-01 13:30:11 +0200

    add img for card + border

--- src/components/SectionPerformances.js
+++ src/components/SectionPerformances.js
@@ -4,17 +4,17 @@ import SectionType from './SectionTypePerf';
 
 const theme = [
     {
-        image : "",
+        image : "https://visionofcolour.com/wp-content/uploads/2019/12/myrtle-corbin-4-legged-freak.jpg",
         title : "Laugh",
         text : "As an adult, come and discover our irresistible clowns, between practical jokes and pranks let yourself be carried away by their joy and fall back into childhood."
     },
     {
-        image : "",
+        image : "https://i.pinimg.com/originals/e7/c9/72/e7c9723bd6704ff3f380543cd19b0905.jpg",
         title : "Dream",
         text : "Let yourself be carried away in a world where the real and the imaginary are one, in the company of our talented magicians, discover a wonderful world limited only by your imagination."
     },
     {
-        image : "",
+        image : "https://i.imgur.com/5dA1MYu.png",
         title : "Marvel at",
         text : "Tame the untameable in the company of our tamers, between roar and razor-sharp claws, watch these fierce felines turn into sweet kittens."
     }
