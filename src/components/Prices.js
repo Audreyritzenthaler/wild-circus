0d2b2b5752ea7a622618e0e004fcf8f22cb5869c
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-04-01 15:54:09 +0200

    create tab

---
+++ src/components/Prices.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+const Prices = () => {
+    return (
+        <div className="mytab">
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan="">Prices</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td>Adult</td>
+                        <td>Children -12 years old</td>
+                        <td>group (+10 person)</td>
+                        <td>School</td>
+                    </tr>
+                    <tr>
+
+                    </tr>
+                    <tr>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Prices;
